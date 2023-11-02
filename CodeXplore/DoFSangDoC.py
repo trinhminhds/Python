ec73f4ba8a159d3cfbbb9d996f2dc9ff5a993252
trinhminhds <trinhngocminh1999@gmail.com>
2023-11-02 23:15:51 +0700

    update

--- CodeXplore/DoFSangDoC.py
+++ CodeXplore/DoFSangDoC.py
@@ -1,9 +1,13 @@
 def FsangC():
-    do_C = int(input("Please enter C :"))
-    if do_C:
-        do_F = 9 * do_C / 5 + 32
-
-        print(f"{do_C} C is converted to {do_F} F")
+    while True:
+        do_C = int(input("Please enter C :"))
+        if do_C:
+            do_F = 9 * do_C / 5 + 32
+            print(f"{do_C} C is converted to {do_F} F")
+            break
+        else:
+            print("C input is emtpy")
+            continue
 
 
 def main():
