863d1984432216a00a3bbc5db793a934777ac0da
trinhminhds <trinhngocminh1999@gmail.com>
2024-11-22 14:40:28 +0700

    update

---
+++ Python_Programming_Tutorials/Working_With_Json.py
@@ -0,0 +1,7 @@
+def nhan(a, b):
+    return a * b
+
+
+if __name__ == "__main__":
+    t = nhan(10, 20)
+    print("so luong", t)
---
+++ Python_Programming_Tutorials/callnew.py
@@ -0,0 +1,5 @@
+import Working_With_Json
+
+t = Working_With_Json.nhan(10, 10)
+
+print("so luong:", t)
